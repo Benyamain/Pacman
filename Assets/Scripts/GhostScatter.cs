532c03fa3bf1ca02574f9caf5d08af06b61f1d88
Benyamain <yacoobbenyamain@gmail.com>
2023-06-02 02:20:59 -0400

    Ghost scatter behavior is implemented!! Got a few more behaviors to implement :smile:

--- Assets/Scripts/GhostScatter.cs
+++ Assets/Scripts/GhostScatter.cs
@@ -4,5 +4,28 @@ using UnityEngine;
 
 public class GhostScatter : GhostBehavior
 {
-    
+    private void OnDisable() {
+        this.ghost.chase.Enable();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Node node = other.GetComponent<Node>();
+
+        // This as in scatter
+        if (node != null && this.enabled && !this.ghost.frightened.enabled) {
+            int index = Random.Range(0, node.availableDirections.Count);
+
+            if (node.availableDirections[index] == -this.ghost.movement.direction && node.availableDirections.Count > 1) {
+                // Change it to the next available direction so does not reverse back in previous position
+                index++;
+
+                if (index >= node.availableDirections.Count) {
+                    index = 0;
+                }
+            }
+
+            this.ghost.movement.SetDirection(node.availableDirections[index]);
+        }
+
+    }
 }
