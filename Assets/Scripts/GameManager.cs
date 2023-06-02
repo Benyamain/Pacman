8c4d44277c92fda09b08d31b2d8d8a9b63ecf90f
Benyamain <yacoobbenyamain@gmail.com>
2023-06-02 17:46:05 -0400

    SFX included in the game! Now, will work on fruit implementation and their point system!

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,107 +15,170 @@ public class GameManager : MonoBehaviour
     public Text scoreText;
     public Text livesText;
     public Text getReadyText;
+    public AudioSource pelletSFX;
+    public AudioSource ghostEatenSFX;
+    public AudioSource frightenedSFX;
+    public AudioSource pacmanDeathSFX;
+    public AudioSource getReadySFX;
+    private bool canPlay = false;
 
-    private void Awake() {
+    private void Awake()
+    {
+        if (!getReadySFX.isPlaying)
+        {
+            StartCoroutine(EnablePlayAfterSFX());
+        }
+
         getReadyText.enabled = true;
 
-        for (int i = 0; i < this.ghosts.Length; i++) {
+        for (int i = 0; i < this.ghosts.Length; i++)
+        {
             this.ghosts[i].gameObject.SetActive(false);
         }
 
         this.pacman.gameObject.SetActive(false);
     }
 
-    private void Update() {
-        if (Input.anyKeyDown) {
+    private IEnumerator EnablePlayAfterSFX()
+    {
+        getReadySFX.Play();
+
+        yield return new WaitForSeconds(getReadySFX.clip.length);
+
+        canPlay = true;
+    }
+
+    private void Update()
+    {
+        if (canPlay && Input.anyKeyDown)
+        {
             getReadyText.enabled = false;
-            
-            if (this.lives <= 0) {
+
+            if (this.lives <= 0)
+            {
                 NewGame();
             }
         }
     }
 
-    private void NewGame() {
+    private void NewGame()
+    {
         SetScore(0);
         SetLives(3);
         NewRound();
     }
 
-    private void NewRound() {
+    private void NewRound()
+    {
         gameOverText.enabled = false;
 
-        foreach (Transform pellet in this.pellets) {
+        foreach (Transform pellet in this.pellets)
+        {
             pellet.gameObject.SetActive(true);
         }
 
         ResetState();
     }
 
-    private void ResetState() {
+    private void ResetState()
+    {
         ResetGhostMultiplier();
 
-        for (int i = 0; i < this.ghosts.Length; i++) {
+        for (int i = 0; i < this.ghosts.Length; i++)
+        {
             this.ghosts[i].ResetState();
         }
 
         this.pacman.ResetState();
     }
 
-    private void GameOver() {
+    private void GameOver()
+    {
         gameOverText.enabled = true;
 
-        for (int i = 0; i < this.ghosts.Length; i++) {
+        for (int i = 0; i < this.ghosts.Length; i++)
+        {
             this.ghosts[i].gameObject.SetActive(false);
         }
 
         this.pacman.gameObject.SetActive(false);
     }
 
-    private void SetScore(int score) {
+    private void SetScore(int score)
+    {
         this.score = score;
         scoreText.text = score.ToString().PadLeft(2, '0');
     }
 
-    private void SetLives(int lives) {
+    private void SetLives(int lives)
+    {
         this.lives = lives;
         livesText.text = "x" + lives.ToString();
     }
 
     // Will be triggered by other scripts so public
-    public void GhostEaten(Ghost ghost) {
+    public void GhostEaten(Ghost ghost)
+    {
         int points = ghost.points * this.ghostMultiplier;
         SetScore(this.score + points);
+
+        if (!ghostEatenSFX.isPlaying)
+        {
+            ghostEatenSFX.Play();
+        }
         this.ghostMultiplier++;
     }
 
-    public void PacmanEaten() {
+    public void PacmanEaten()
+    {
         pacman.DeathSequence();
 
         SetLives(this.lives - 1);
 
-        if (this.lives > 0) {
+        if (!pacmanDeathSFX.isPlaying)
+        {
+            pacmanDeathSFX.Play();
+        }
+
+        if (this.lives > 0)
+        {
             Invoke(nameof(ResetState), 3.0f);
         }
-        else {
+        else
+        {
             GameOver();
         }
     }
 
-    public void PelletEaten(Pellet pellet) {
+    public void PelletEaten(Pellet pellet)
+    {
         pellet.gameObject.SetActive(false);
 
         SetScore(this.score + pellet.points);
 
-        if (!HasRemainingPellets()) {
+        if (!pelletSFX.isPlaying)
+        {
+            pelletSFX.Play();
+        }
+
+        if (!HasRemainingPellets())
+        {
             this.pacman.gameObject.SetActive(false);
             Invoke(nameof(NewRound), 3f);
         }
     }
 
-    public void PowerPelletEaten(PowerPellet pellet) {
+    public void PowerPelletEaten(PowerPellet pellet)
+    {
+        if (!frightenedSFX.isPlaying)
+        {
+            frightenedSFX.Play();
+            frightenedSFX.loop = true;
+        }
+
         // Change ghost state to frightened
-        for (int i = 0; i < this.ghosts.Length; i++) {
+        for (int i = 0; i < this.ghosts.Length; i++)
+        {
             this.ghosts[i].frightened.Enable(pellet.duration);
         }
 
@@ -123,12 +186,23 @@ public class GameManager : MonoBehaviour
         // Timer in progress will get cancelled and start over again
         CancelInvoke();
         Invoke(nameof(ResetGhostMultiplier), pellet.duration);
+        Invoke(nameof(StopFrightenedSFX), pellet.duration);
     }
 
-    private bool HasRemainingPellets() {
-        foreach (Transform pellet in this.pellets) {
+    private void StopFrightenedSFX()
+    {
+        frightenedSFX.Stop();
+        frightenedSFX.loop = false;
+    }
+
+
+    private bool HasRemainingPellets()
+    {
+        foreach (Transform pellet in this.pellets)
+        {
             // Game object is active so there are remaining pellets
-            if (pellet.gameObject.activeSelf) {
+            if (pellet.gameObject.activeSelf)
+            {
                 return true;
             }
         }
@@ -136,7 +210,8 @@ public class GameManager : MonoBehaviour
         return false;
     }
 
-    private void ResetGhostMultiplier() {
+    private void ResetGhostMultiplier()
+    {
         this.ghostMultiplier = 1;
     }
 }
